61750a15094716978e172ef208b0fcb414c56f83
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-03-01 10:49:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = "https://github.com/TechShreyash/SiestaXMusic"
+UPSTREAM_REPO = "https://github.com/Gameralpha/MYPERSONALEDIT"
 UPSTREAM_BRANCH = "main"
 
-SUPPORT_CHANNEL = "https://t.me/TechZBots"
-SUPPORT_GROUP = "https://t.me/TechZBots_Support"
+SUPPORT_CHANNEL = "https://t.me/Official_Devil_Network"
+SUPPORT_GROUP = "https://t.me/Official_Devil_Support"
 
 THUMBNAIL = getenv("THUMB_LINK") 
 
